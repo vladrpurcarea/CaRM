1bdb4a2bbcc2a89d68c27059e6618fa975a243ac
Vlad Purcarea <vpurcarea@simscale.com>
2020-12-29 03:19:47 +0100

    add view contact request page

--- web/carm/static/js/fe.js
+++ web/carm/static/js/fe.js
@@ -14,3 +14,19 @@ function getCookieValue(a) {
 function parseTimestamp(t) {
     return new Date((t-2208988800)*1000);
 }
+
+function getParameters() {
+    var prmstr = window.location.search.substr(1);
+    return prmstr != null && prmstr != "" ? transformToAssocArray(prmstr) : {};
+}
+
+function transformToAssocArray( prmstr ) {
+    var params = {};
+    var prmarr = prmstr.split("&");
+    for ( var i = 0; i < prmarr.length; i++) {
+        var tmparr = prmarr[i].split("=");
+        params[tmparr[0]] = tmparr[1];
+    }
+    return params;
+}
+
