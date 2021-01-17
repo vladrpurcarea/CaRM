d4e1f87661aa2a463ebb039b8b04d5f49f34bad7
Vlad Purcarea <vpurcarea@simscale.com>
2021-01-17 16:29:45 +0100

    fix viagra regex

--- src/spam.lisp
+++ src/spam.lisp
@@ -5,7 +5,7 @@
 (defvar *url-regex*
   (ppcre:create-scanner "(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})"))
 (defvar *viagra-regex*
-  (ppcre:create-scanner "[^a-zA-Z]viagra[^a-zA-Z]"))
+  (ppcre:create-scanner "(^|[^a-zA-Z])viagra([^a-zA-Z]|$)"))
 (defvar *russian-regex*
   (ppcre:create-scanner "\\p{Cyrillic}"))
 
